5e2d3e78fbf300fef0ae899d521d96e41f420cf9
Maksim Odnoletkov <odnoletkov@mail.ru>
2024-01-20 19:22:29 +0000

    Continue

--- ExplorePHPicker/AppDelegate.swift
+++ ExplorePHPicker/AppDelegate.swift
@@ -56,21 +56,61 @@ extension Controller: PHPickerViewControllerDelegate {
 
 extension Controller: UISheetPresentationControllerDelegate {
     func sheetPresentationControllerDidChangeSelectedDetentIdentifier(_ sheet: UISheetPresentationController) {
-        if sheet.selectedDetentIdentifier == .large  {
-            sheet.detents = [.large()]
-            let pickerController = sheet.presentedViewController as! PHPickerViewController
-            sheet.animateChanges {
-                var update = PHPickerConfiguration.Update()
-                update.edgesWithoutContentMargins = pickerController.configuration.edgesWithoutContentMargins
-                update.edgesWithoutContentMargins?.subtract(.top)
-                pickerController.updatePicker(using: update)
+        print("\(#function)")
 
-                DispatchQueue.main.async {
-                    pickerController.scrollToInitialPosition()
-                }
-
-                sheet.prefersGrabberVisible = false
-            }
+        guard sheet.selectedDetentIdentifier == .large && sheet.detents != [.large()] else {
+            return
         }
+
+        let pickerController = sheet.presentedViewController as! PHPickerViewController
+        pickerController.updatePicker(using: {
+            var update = PHPickerConfiguration.Update()
+            update.edgesWithoutContentMargins = pickerController.configuration.edgesWithoutContentMargins
+            update.edgesWithoutContentMargins?.subtract(.top)
+            return update
+        }())
+        sheet.prefersGrabberVisible = false
+    }
+}
+
+extension Controller: UIAdaptivePresentationControllerDelegate {
+    func adaptivePresentationStyle(for controller: UIPresentationController) -> UIModalPresentationStyle {
+        print("\(#function)")
+        return .automatic
+    }
+
+    func adaptivePresentationStyle(for controller: UIPresentationController, traitCollection: UITraitCollection) -> UIModalPresentationStyle {
+        print("\(#function)")
+        return .automatic
+    }
+
+    func presentationController(_ presentationController: UIPresentationController, prepare adaptivePresentationController: UIPresentationController) {
+        print("\(#function)")
+    }
+
+    func presentationController(_ controller: UIPresentationController, viewControllerForAdaptivePresentationStyle style: UIModalPresentationStyle) -> UIViewController? {
+        print("\(#function)")
+        return nil
+    }
+
+    func presentationController(_ presentationController: UIPresentationController, willPresentWithAdaptiveStyle style: UIModalPresentationStyle, transitionCoordinator: UIViewControllerTransitionCoordinator?) {
+        print("\(#function)")
+    }
+
+    func presentationControllerShouldDismiss(_ presentationController: UIPresentationController) -> Bool {
+        print("\(#function)")
+        return true
+    }
+
+    func presentationControllerWillDismiss(_ presentationController: UIPresentationController) {
+        print("\(#function)")
+    }
+
+    func presentationControllerDidDismiss(_ presentationController: UIPresentationController) {
+        print("\(#function)")
+    }
+
+    func presentationControllerDidAttemptToDismiss(_ presentationController: UIPresentationController) {
+        print("\(#function)")
     }
 }
